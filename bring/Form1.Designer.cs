e45aff0d5129493a9f1f5a7636654c7735a02f6f
rainshowers0 <batjargal53@yahoo.com>
2022-11-18 19:46:39 +0800

    Filestream

--- bring/Form1.Designer.cs
+++ bring/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -61,11 +64,43 @@
             this.textBox1.Size = new System.Drawing.Size(224, 144);
             this.textBox1.TabIndex = 3;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(488, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(488, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(569, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -81,5 +116,8 @@
         private Button button1;
         private Button button2;
         private TextBox textBox1;
+        private ComboBox comboBox1;
+        private Button button3;
+        private Button button4;
     }
 }
--- bring/Form1.cs
+++ bring/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace bring
 {
     public partial class Form1 : Form
@@ -36,6 +38,29 @@ namespace bring
                 MessageBox.Show(sr.Peek().ToString());
             }
             sr.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(@"C:\Users\User\Desktop\");
+            foreach(string file in files)
+            {
+                if(Path.GetExtension(file) == ".txt") comboBox1.Items.Add(file);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string filename = comboBox1.Text;
+            FileStream FS = File.OpenWrite(filename);
+            byte[] mytext = new UTF8Encoding(true).GetBytes(textBox1.Text);
+            FS.Write(mytext, 0, mytext.Length);
+            FS.Close();
         }
     }
 }
