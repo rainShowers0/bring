81861c664f7f485a91c1ea39f25f0fe0b6fb5b72
rainshowers0 <batjargal53@yahoo.com>
2022-12-02 20:10:02 +0800

    s

--- bring/Form1.cs
+++ bring/Form1.cs
@@ -1,6 +1,4 @@
-using System.Text;
-
-namespace bring
+﻿namespace bring
 {
     public partial class Form1 : Form
     {
@@ -11,8 +9,44 @@ namespace bring
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Mainform myform = new Mainform();
-            myform.Show();
+            if (textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("хэрэглэгчийн нэр оруулна уу");
+            }
+            else if (textBox2.Text.Trim() == "")
+            {
+                MessageBox.Show("нууц үгээ оруулна уу");
+            }
+            else
+            {
+                StreamReader sr = new StreamReader(@"..\..\..\txtfiles\myUser.txt");
+                string username;
+                string password;
+                bool isMyUser = false;
+                while (sr.Peek() > -1)
+                {
+                    username = sr.ReadLine();
+                    password = sr.ReadLine();
+                    if (username == textBox1.Text.Trim() && password == textBox2.Text.Trim())
+                    {
+                        isMyUser = true;
+                        Mainform myForm = new Mainform();
+                        myForm.Show();
+                        this.Hide();
+                    }
+                }
+                sr.Close();
+                if (isMyUser == false)
+                {
+                    MessageBox.Show("buruu");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
         }
     }
 }
